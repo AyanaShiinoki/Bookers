bff6fc5b37cbf7ae320f0d291d1cdeb59eb7e754
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-11-21 11:19:59 +0900

    [create]form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,22 @@ class BooksController < ApplicationController
   end
 
   def new
+    @book=Book.new
+  end
+
+  def create
+    book=Book.new
+    book.save(book_params)
+    redirect_to  books_path
   end
 
   def edit
   end
+
+private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+
 end
