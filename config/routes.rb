f6646566d0345e06bcbfd6859444dc0efd328bf6
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-11-21 11:01:19 +0900

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/top'
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
+	resources :books
+	root to:'books#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
