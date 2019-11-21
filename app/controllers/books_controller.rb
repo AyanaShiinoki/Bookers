b92144ae79ce657f9b67935ef062efa1f1ef1c3d
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-11-21 12:57:08 +0900

    [create]index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,9 +3,12 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books=Book.all
+    @book=Book.new
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def new
@@ -13,12 +16,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new
-    book.save(book_params)
+    book=Book.new(book_params)
+    book.save
     redirect_to  books_path
   end
 
   def edit
+   
   end
 
 private
