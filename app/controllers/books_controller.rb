7fcefa9fb697a9760fe230353516a8958f14d257
MacUser <macuser@MacUsernoMacBook-Pro.local>
2019-12-01 13:35:54 +0900

    [finish]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,12 +16,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    if book.save
+    @book=Book.new(book_params)
+    if  @book.save
     flash[:notice]="Book was successfully created."
-    redirect_to  book_path(book.id) 
+    redirect_to  book_path(@book.id)
     else
-    #空欄時の処理
+    @books=Book.all
+    render 'index'
     end
   end
 
@@ -31,12 +32,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    if book.update(book_params)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice2]="Book was successfully updated."
-    redirect_to  book_path(book.id)
+    redirect_to  book_path(@book.id)
     else
     #空欄時の処理
+    render 'edit'
     end
   end
 
